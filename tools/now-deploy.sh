1c04c11cfb628214d9ed225bd6bd27f35318e6cc
Alejandro Oviedo Garcia <alejandro.oviedo.g@gmail.com>
2018-07-15 19:34:44 -0300

    add error handling

--- tools/now-deploy.sh
+++ tools/now-deploy.sh
@@ -11,9 +11,9 @@ write_comment_to_pr () {
 
 write_comment_to_pr "Travis automatic deployment: '${DEPLOY_DOMAIN}'"
 
-echo $TRAVIS_PULL_REQUEST_BRANCH
-
 git diff --name-only gh-pages
+
+curl 'https://cards-dev.twitter.com/validator' -H "cookie: $TWITTER_COOKIE"
 
 # get list of modified files with html extension except cfp.html, speakers.html, conduct.html, index.html
 XARGS_RESULT=$(git diff --name-only gh-pages | grep '.html' | grep -v 'cfp.html' | grep -v 'speakers.html' | grep -v 'conduct.html' | grep -v 'index.html' | \
--- tools/screenshot.js
+++ tools/screenshot.js
@@ -10,46 +10,52 @@ function once(emitter, event) {
 }
 
 (async() => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
 
-  const cookie = process.env.TWITTER_COOKIE;
-  const cookieList = cookie.split('; ').map(elem => {
-    const [name, value ] = elem.split('=');
-    return {
-      value,
-      name,
-      domain: '.twitter.com',
-      expires: Date.now() + 600000000
-    };
-  });
-  await page.setCookie(...cookieList);
-  await page.emulate({
-    viewport: {
-      width: 1024,
-      height: 768
-    },
-    userAgent: 'fruta'
-  });
-  await page.goto('https://cards-dev.twitter.com/validator', {
-    waitLoad: true
-  });
+  try {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
 
-  const textSelector = '#ValidationForm > div > div:nth-child(1) > input.FormControl';
-  const clickSelector = '#ValidationForm > div > div.Grid-cell.u-sizeFill.u-marginTm > input';
-  await page.click(textSelector);
-  await page.keyboard.type(process.argv[2]);
-  await page.click(clickSelector);
-  await page.waitForSelector('#CardPreview > div.CardPreview-preview.js-cardPreview iframe');
+    const cookie = process.env.TWITTER_COOKIE;
+    const cookieList = cookie.split('; ').map(elem => {
+      const [name, value ] = elem.split('=');
+      return {
+        value,
+        name,
+        domain: '.twitter.com',
+        expires: Date.now() + 600000000
+      };
+    });
+    await page.setCookie(...cookieList);
+    await page.emulate({
+      viewport: {
+        width: 1024,
+        height: 768
+      },
+      userAgent: 'fruta'
+    });
+    await page.goto('https://cards-dev.twitter.com/validator', {
+      waitLoad: true
+    });
 
-  const frame = await once(page, 'framenavigated');
-  await frame.waitForSelector('body > div > div > a > div.SummaryCard-image > div > div > img');
-  await timeout(800);
+    const textSelector = '#ValidationForm > div > div:nth-child(1) > input.FormControl';
+    const clickSelector = '#ValidationForm > div > div.Grid-cell.u-sizeFill.u-marginTm > input';
+    await page.click(textSelector);
+    await page.keyboard.type(process.argv[2]);
+    await page.click(clickSelector);
+    await page.waitForSelector('#CardPreview > div.CardPreview-preview.js-cardPreview iframe');
 
-  const fileName = `${Date.now()}.png`;
-  await page.screenshot({path: fileName});
-  console.log(fileName);
+    const frame = await once(page, 'framenavigated');
+    await frame.waitForSelector('body > div > div > a > div.SummaryCard-image > div > div > img');
+    await timeout(800);
 
-  await page.close();
-  await browser.close();
+    const fileName = `${Date.now()}.png`;
+    await page.screenshot({path: fileName});
+    console.log(fileName);
+
+    await page.close();
+    await browser.close();
+  } catch(err) {
+    console.log('There was an error:', err);
+    process.exit(1);
+  }
 })();
