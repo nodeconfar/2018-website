e9a42bc0948daeb39df3db425cb4c44a8bfeba1b
Alejandro Oviedo Garcia <alejandro.oviedo.g@gmail.com>
2018-07-15 16:11:04 -0300

    add puppeteer

---
+++ tools/now-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DEPLOY_DOMAIN=$(now --token $NOW_TOKEN --public --static)
+npm run lh $DEPLOY_DOMAIN
+
+GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+# curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
+
+write_comment_to_pr () {
+    curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data "{\"body\": \"$1\"}"
+}
+
+write_comment_to_pr "Travis automatic deployment: '${DEPLOY_DOMAIN}'"
+
+# get list of modified files with html extension except cfp.html, speakers.html, conduct.html, index.html
+XARGS_RESULT=$(git diff --name-only gh-pages...$TRAVIS_BRANCH | grep '.html' | grep -v 'cfp.html' | grep -v 'speakers.html' | grep -v 'conduct.html' | grep -v 'index.html' | \
+xargs -L1 -I % sh -c 'printf "%;"; ./imgur.sh $(node tools/screenshot.js %)')
+
+echo "$XARGS_RESULT" | while read each
+do
+  IFS=';' read -r -a line <<< "$each"
+  write_comment_to_pr "File modified ${line[0]}: \n ![](${line[1]})"
+done
---
+++ tools/screenshot.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+function once(emitter, event) {
+  return new Promise(resolve => emitter.once(event, resolve));
+}
+
+(async() => {
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+
+  const cookie = process.env.TWITTER_COOKIE;
+  const cookieList = cookie.split('; ').map(elem => {
+    const [name, value ] = elem.split('=');
+    return {
+      value,
+      name,
+      domain: '.twitter.com',
+      expires: Date.now() + 600000000
+    };
+  });
+  await page.setCookie(...cookieList);
+  await page.emulate({
+    viewport: {
+      width: 1024,
+      height: 768
+    },
+    userAgent: 'fruta'
+  });
+  await page.goto('https://cards-dev.twitter.com/validator', {
+    waitLoad: true
+  });
+
+  const textSelector = '#ValidationForm > div > div:nth-child(1) > input.FormControl';
+  const clickSelector = '#ValidationForm > div > div.Grid-cell.u-sizeFill.u-marginTm > input';
+  await page.click(textSelector);
+  await page.keyboard.type(process.argv[2]);
+  await page.click(clickSelector);
+  await page.waitForSelector('#CardPreview > div.CardPreview-preview.js-cardPreview iframe');
+
+  const frame = await once(page, 'framenavigated');
+  await frame.waitForSelector('body > div > div > a > div.SummaryCard-image > div > div > img');
+  await timeout(500);
+
+  const fileName = `${Date.now()}.png`;
+  await page.screenshot({path: fileName});
+  console.log(fileName);
+
+  await page.close();
+  await browser.close();
+})();
