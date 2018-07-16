6623135efb6750e688d726810dd888afdbedff72
Alejandro Oviedo Garcia <alejandro.oviedo.g@gmail.com>
2018-07-15 21:05:57 -0300

    post single comment

--- tools/now-deploy.sh
+++ tools/now-deploy.sh
@@ -8,7 +8,7 @@ write_comment_to_pr () {
     curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data "{\"body\": \"$1\"}"
 }
 
-write_comment_to_pr "Travis automatic deployment: ${DEPLOY_DOMAIN}"
+PR_COMMENT="Travis automatic deployment: ${DEPLOY_DOMAIN}\n"
 
 # get list of modified files with html extension except cfp.html, speakers.html, conduct.html, index.html
 XARGS_RESULT=$(git diff --name-only gh-pages | grep '.html' | grep -v 'cfp.html' | grep -v 'speakers.html' | grep -v 'conduct.html' | grep -v 'index.html' | \
@@ -17,5 +17,7 @@ xargs -L1 -I % sh -c "printf \"%;\"; node tools/screenshot.js $DEPLOY_DOMAIN/%")
 echo "$XARGS_RESULT" | while read each
 do
   IFS=';' read -r -a line <<< "$each"
-  write_comment_to_pr "File modified ${line[0]}: \n ![]($(./tools/imgur.sh ${line[1]}))"
+  PR_COMMENT+="File modified ${line[0]}: \n ![]($(./tools/imgur.sh ${line[1]}))\n"
 done
+
+write_comment_to_pr $PR_COMMENT
