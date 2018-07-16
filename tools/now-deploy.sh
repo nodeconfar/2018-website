3ef07b220633f580dace3dc4e6b6c36f287680d5
Alejandro Oviedo Garcia <alejandro.oviedo.g@gmail.com>
2018-07-15 21:00:53 -0300

    cleanup

--- tools/now-deploy.sh
+++ tools/now-deploy.sh
@@ -3,7 +3,6 @@ DEPLOY_DOMAIN=$(now --token $NOW_TOKEN --public --static)
 npm run lh $DEPLOY_DOMAIN
 
 GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
-# curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
 
 write_comment_to_pr () {
     curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data "{\"body\": \"$1\"}"
@@ -11,17 +10,9 @@ write_comment_to_pr () {
 
 write_comment_to_pr "Travis automatic deployment: ${DEPLOY_DOMAIN}"
 
-git diff --name-only gh-pages
-
-echo 'testing twitter cookies'
-curl 'https://cards-dev.twitter.com/validator' -H "cookie: $TWITTER_COOKIE" -I
-curl 'https://cards-dev.twitter.com/validator' -H "cookie: $TWITTER2" -I
-
 # get list of modified files with html extension except cfp.html, speakers.html, conduct.html, index.html
 XARGS_RESULT=$(git diff --name-only gh-pages | grep '.html' | grep -v 'cfp.html' | grep -v 'speakers.html' | grep -v 'conduct.html' | grep -v 'index.html' | \
 xargs -L1 -I % sh -c "printf \"%;\"; node tools/screenshot.js $DEPLOY_DOMAIN/%")
-
-echo $XARGS_RESULT
 
 echo "$XARGS_RESULT" | while read each
 do
